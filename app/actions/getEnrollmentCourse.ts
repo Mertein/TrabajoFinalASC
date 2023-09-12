f6468bdce5cdcc6883ff311988b1351a09982b8f
Martín Eduardo Aguayo <martineduardoaguayo@gmail.com>
2023-09-11 22:33:44 -0300

    progress

--- app/actions/getEnrollmentCourse.ts
+++ app/actions/getEnrollmentCourse.ts
@@ -1,18 +1,106 @@
+import prisma from "../../lib/prismadb";
 async function getEnrollmentCourse() {
   try {
-    const res = await fetch('http://localhost:3000/api/enrollmentCourse', {
-      cache: 'reload',
+    const fieldsPromise = prisma.enrollment_course.findMany({
+      select: {
+        enrollment_id: true,
+        feedback_course: true,
+        payment_status  : true,
+        enrollment_date: true,
+        completion_status: true,
+        user_id: true,
+        course_id: true,
+        files: {
+          where: {
+            identifier: "studentCertificateCourse"
+          },
+          select: {
+            id: true,
+            name: true,
+            type: true,
+            identifier: true,
+            path: true,
+            user_id: true,
+          },
+        },
+        course: {
+          select: {
+            course_id: true,
+            course_name: true,
+            price_course: true,
+            description_course: true,
+            user_id: true,
+            category_id: true,
+            id_branch: true,
+            end_date: true,
+            start_date: true,
+            isFree: true,
+            isVirtual: true,
+            description: true,
+            usser: {
+              select: {
+                first_name: true,
+                last_name: true,
+                dni: true,
+              }
+            },
+            category_course: true,
+          },
+        },
+        usser: {
+          select: {
+            files: {
+              where: {
+                identifier: "studentCertificateCourse",
+              },
+              select: {
+                id: true,
+                name: true,
+                type: true,
+                identifier: true,
+                path: true,
+                user_id: true,
+              },
+            },
+          first_name: true,
+          last_name: true,
+          dni: true,
+          },
+        },
+        grade: {
+          select: {
+            grade_id: true,
+            value: true,
+          },
+        },
+      },
     });
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch Data');
-    }
+    const [fields] = await Promise.all([fieldsPromise]);
 
-    return res.json();
+    const convertedFields = fields.map((field) => {
+      return {
+        ...field,
+        usser: {
+          ...field.usser,
+          dni: field.usser.dni.toString(), // Convertir Decimal a cadena
+        },
+        course: {
+          ...field.course,
+          usser: {
+            ...field.course.usser,
+            dni: field.course.usser.dni.toString(), // Convertir Decimal a cadena
+          }
+        },
+      };
+    });
+
+    return (convertedFields as any) || [];
   } catch (error) {
-    console.error('Error al obtener los datos:', error);
-    throw error; // Re-lanzar el error para que se maneje en otro lugar si es necesario.
+    console.error(error);
+    throw error;
   }
 }
+
 
 export default getEnrollmentCourse;
